da6dd3aee5e5e722a113c5561c98bf9a65bf39d9
klBiazon <klbiazon@dynamicobjx.com>
2016-11-22 18:29:08 +0800

    Added directions

--- client/app/store/store.controller.js
+++ client/app/store/store.controller.js
@@ -2,22 +2,85 @@
 (function(){
 
   class StoreComponent {
-    constructor($scope, API) {
-        $scope.positions = [];
-        navigator.geolocation.getCurrentPosition(function (position) {
-          API.get(`stores/location/near/` + position.coords.longitude + `/` + position.coords.latitude, '', function(res, err) {
-            if (!err) {
-                var location = [];
-                for (var i = 0; i < res.length; i++) {
-                  location = res[i].geoLocation.coordinates;
-                  $scope.positions.push([location[1], location[0]]);
-                }
-            } else {
-              console.log(err);
-            }
-          });
-        });
+    constructor($scope, $timeout, API, NgMap) {
+      $scope.stores = [];
+      $scope.current = [];
+      $scope.positions = [];
+      $scope.message = 'Your location is based on your current location';
+      $scope.option = true;
+      $scope.showDirection = false;
+      $scope.storeDirection = '';
 
+      function searchNearBy (place) {
+        $scope.positions = [];
+        if($scope.option) {
+          navigator.geolocation.getCurrentPosition(function (position) {
+            $scope.current = [position.coords.latitude, position.coords.longitude];
+            $timeout(function () {
+              $scope.setNearByStores(position.coords);
+            }, 1000);
+          });
+        } else {
+          $scope.current = [place.latitude, place.longitude];
+          $timeout(function () {
+            $scope.setNearByStores(place);
+          }, 1000);
+        }
+      }
+
+      $scope.setNearByStores = function(position) {
+        API.get(`stores/location/near/` + position.longitude + `/` + position.latitude, '', function(res, err) {
+          if (!err) {
+            $scope.stores = res;
+            var location = [];
+            for (var i = 0; i < res.length; i++) {
+              location = res[i].geoLocation.coordinates;
+              $scope.positions.push([location[1], location[0]]);
+            }
+          } else {
+            console.log(err);
+          }
+        });
+      };
+
+      $scope.optionClicked = function() {
+        $scope.showDirection = false;
+        $scope.message = '';
+        $scope.stores = [];
+        if ($scope.option) {
+          searchNearBy();
+          $scope.message = 'Your location is based on your current location';
+          $scope.address = '';
+          $scope.storeDirection = '';
+        } else {
+          $scope.positions = [];
+        }
+      };
+
+      $scope.placeChanged = function() {
+        var place = {
+          longitude: this.getPlace().geometry.location.lng(),
+          latitude: this.getPlace().geometry.location.lat()
+        }
+        searchNearBy(place);
+      };
+
+      $scope.bounce = function() {
+        this.setAnimation(google.maps.Animation.BOUNCE);
+      };
+
+      $scope.direction = function(store) {
+        if ($scope.storeDirection === store._id) {
+          $scope.showDirection = false;
+          $scope.storeDirection = '';
+        } else {
+          $scope.destination = store.address;
+          $scope.storeDirection = store._id;
+          $scope.showDirection = true;
+        }
+      };
+
+      searchNearBy();
     }
   }
 
